e3bef6a315030f7f3b1c89c20782317fe0e78b11
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-17 19:42:34 +0100

    Add files via upload

---
+++ m03programacio/uf2/UtilsLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿using UtilsLibrary;
+
+namespace MyNamespace
+{
+    public class Programa
+
+    {
+        public static void Main()
+        {
+            int num;
+            const string Msg = "Introdueix un valor natural";
+            const string MsgResult = "El valor introduït és: {0}";
+            do
+            {
+                Console.WriteLine(Msg);
+                num = Convert.ToInt32(Console.ReadLine());
+            } while (!Utils.ValidateNum(num));
+            Console.WriteLine(MsgResult, num);
+        }
+    }
+}
---
+++ m03programacio/uf2/UtilsLibrary/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace UtilsLibrary
+{
+    public class Utils
+    {
+        public static bool ValidateNum(int num)
+        {
+            return num > 0;
+        }
+    }
+}
+
