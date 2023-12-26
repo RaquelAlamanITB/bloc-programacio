ba6c65578e8748eacb07e9d78cd1d0f31002bd91
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-26 20:27:16 +0100

    Add files via upload

---
+++ m03programacio/uf2/Recursivitat/HanoiTowers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HanoiTower
+{
+    class Program
+    {
+        private const string MsgMove = "Mou el disc de {0} a {1}.";
+        static void SolveTowers(int n, char fromRod, char toRod, char auxRod)
+        {
+            if (n > 0)
+            {
+                SolveTowers(n - 1, fromRod, auxRod, toRod);
+                Console.WriteLine(MsgMove, fromRod, toRod);
+                SolveTowers(n - 1, auxRod, toRod, fromRod);
+
+            }
+        }
+
+        //  Driver method 
+        public static void Main()
+        {
+            int N = 3;
+            SolveTowers(N, 'A', 'C', 'B');
+        }
+    }
+}
