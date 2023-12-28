229de9127a4569ce8679ab11a271a63fb5222de2
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-28 13:58:55 +0100

    Update FibonacciSeries.cs

--- m03programacio/uf2/Recursivitat/FibonacciSeries.cs
+++ m03programacio/uf2/Recursivitat/FibonacciSeries.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 namespace FibonacciSeries
 {
-    public class Program
-    {
+    public class Program {
+        public const string Msg = "Introdueix la mida de la seqüència de Fibonacci: ";
         public static int FibonacciSeries(int n)
         {
-            if (n == 0) return 0;   
-            if (n == 1) return 1;   
+            if (n == 0) return 0;
+            if (n == 1) return 1;
             return FibonacciSeries(n - 1) + FibonacciSeries(n - 2);
         }
         public static void Main()
         {
-            Console.Write("Enter the length of the Fibonacci Series: ");
+            Console.WriteLine(Msg);
             int length = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < length; i++)
             {
