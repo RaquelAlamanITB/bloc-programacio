951808c6efa9fa616accbfd1aa5ef8ae359ac97b
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-28 13:34:56 +0100

    Add files via upload

---
+++ m03programacio/uf2/Recursivitat/PotenciaRec.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Recursion {
+    public class Program {
+
+        public static int PowRec(int m, int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return m * PowRec(m, n - 1);
+            }
+        }
+
+        public static void Main() {
+            Console.WriteLine("Resultat potència recursiva: {0}", PowRec(2,1));
+        }
+    }
+}
+
+
