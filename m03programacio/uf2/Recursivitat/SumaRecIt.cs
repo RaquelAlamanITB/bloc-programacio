702bf0cd07fdb1b6fa653931d9db56c659393899
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-28 13:20:09 +0100

    Add files via upload

---
+++ m03programacio/uf2/Recursivitat/SumaRecIt.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Recursion {
+    public class Program {
+        public static int SumaIt(int n)
+        {
+            int num = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                num += i;
+            }
+            return num;
+        }
+
+        public static int SumaRec(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n + SumaRec(n - 1);
+            }
+        }
+
+        public static void Main() {
+            Console.WriteLine("Resultat suma iterativa:", SumaIt(4));
+            Console.WriteLine("Resultat suma recursiva:", SumaRec(4));
+        }
+    }
+}
+
+
