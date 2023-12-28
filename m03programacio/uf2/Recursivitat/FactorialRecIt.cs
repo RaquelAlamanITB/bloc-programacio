62eed44525a685fca9ab8a48f9223332902ef025
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-28 13:47:42 +0100

    Add files via upload

---
+++ m03programacio/uf2/Recursivitat/FactorialRecIt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Recursion {
+    public class Program {
+
+        public static int FactorialIt(int n)
+        {
+            int count = 1, val = 1;
+            while (val <= n) 
+            {
+                count *= val;
+                val++;
+            }
+            return count;
+        }
+        public static int FactorialRec(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else 
+            {
+                return n * FactorialRec(n - 1);
+            }
+        }
+
+        public static void Main() {
+            Console.WriteLine("Resultat Factorial iteratiu: {0}", FactorialIt(4));
+            Console.WriteLine("Resultat Factorial recursiu: {0}", FactorialRec(4));
+        }
+    }
+}
+
+
