7145027f489f9246b9f00adebd2c32a438dc481a
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2023-12-29 14:46:42 +0100

    Add files via upload

---
+++ m03programacio/uf2/ParsArgs/ParseArgs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ArgsParser
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ParseArgs(args);
+            Console.WriteLine(GetConsole(args[0], args[1]));
+            SumArgs(args);
+         
+        }
+        public static void SumArgs(string[] args)
+        {
+            Console.WriteLine("Suma: {0}", Convert.ToInt32(args[0]) + Convert.ToInt32(args[1]));
+        }
+        public static void ParseArgs(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+        }
+        public static string GetConsole(string arg, string inArg)
+        {
+            return arg.Contains(inArg) ? "Sí que el conté" : "No el conté";
+        }
+    }
+}
