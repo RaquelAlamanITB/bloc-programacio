20d8743ed76566c9893eb4804512dd427d53ecaa
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-22 20:47:23 +0100

    Add files via upload

---
+++ m03programacio/uf3/StreamReaderProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileHandling("file.txt");
+        }
+
+        public static void FileHandling(string file)
+        {
+            string path = Path.GetFullPath("..\\..\\..\\files\\"+file);
+            StreamReader sr = new StreamReader(path);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
