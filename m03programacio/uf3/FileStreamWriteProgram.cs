64e11430482aeea3ea27af5465956544919e1cda
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-22 23:09:29 +0100

    Add files via upload

---
+++ m03programacio/uf3/FileStreamWriteProgram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileHandling("file.txt");
+        }
+
+        public static void FileHandling(string file)
+        {
+            string path = Path.GetFullPath("..\\..\\..\\files\\" + file);
+            using FileStream fs = File.Create(path);
+
+            byte[] info = new UTF8Encoding(true).GetBytes("Escriptura des de FileStream");
+            fs.Write(info, 0, info.Length);
+        }
+    }
+}
