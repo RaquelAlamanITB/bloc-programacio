dc307acfd767ea42491f88dbc58e8bb9658ed495
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-22 22:01:30 +0100

    Add files via upload

---
+++ m03programacio/uf3/FileWriteProgram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileHandling("file.txt");
+        }
+
+        public static void FileHandling(string file)
+        {
+            string path = Path.GetFullPath("..\\..\\..\\files\\" + file);
+            if (!File.Exists(path))
+            {
+                string createText = "Hello world!" + Environment.NewLine;
+                File.WriteAllText(path, createText);
+
+                string appendText = "Nova línia" + Environment.NewLine;
+                File.AppendAllText(path, appendText);
+            }
+        }
+    }
+}
