e293143c7efd0a8415cbec6defe3157fecfba395
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-22 20:09:58 +0100

    Add files via upload

---
+++ m03programacio/uf3/FileProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileHandling("file.txt");
+        }
+
+        public static void FileHandling(string file)
+        {
+            string path = Path.GetFullPath("..\\..\\..\\files\\"+file);
+            StreamReader sr = File.OpenText(path);
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
