87f0e0a3a98678e651ba9d1bce77d8211261b64a
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-23 13:10:56 +0100

    Add files via upload

---
+++ m03programacio/uf3/StreamWriter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FileHandling
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            FileHandling("file.txt");
+        }
+
+        public static void FileHandling(string file)
+        {
+            string path = Path.GetFullPath(@"..\..\..\files\" + file);
+            //using StreamWriter sw = new StreamWriter(path);
+            //sw.WriteLine("Escriptura amb StreamWriter ");
+
+            using StreamWriter sw = new StreamWriter(path, append: true);
+            sw.WriteLine("Escriptura amb StreamWriter en mode Append");
+        }
+    }
+}
