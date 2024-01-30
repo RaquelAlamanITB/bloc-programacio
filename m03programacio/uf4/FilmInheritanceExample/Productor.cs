ddcefd7086cfadc8d541863b193be1ad99ac5a99
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-30 18:00:15 +0100

    Add files via upload

---
+++ m03programacio/uf4/FilmInheritanceExample/Productor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Xml.Linq;
+
+
+namespace oop
+{
+    public class Productor : AbstractPerson, IPerson
+    {
+        //expression bodied property
+        public override string Name { get => name; set => name = value; }
+        //expression bodied property
+        public override string Surname { get => name; set => name = value; }
+
+        public void DisplayName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public override string GetFullName()
+        {
+            return "Nom: " + Name;
+        }
+    }
+}
