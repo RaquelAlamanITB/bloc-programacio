b9b4d1cc3a8e11bf9c52670e3e0d5ad4e530cb23
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-31 22:46:12 +0100

    Create ActorV3.cs

---
+++ m03programacio/uf4/ActorBasicExample/ActorV3.cs
@@ -0,0 +1,18 @@
+namespace oop
+{
+    public class Actor
+    {
+		    //propietat: mètode per realitzar operacions sobre els atributs
+        //des de c# 3 
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        
+        public Actor(string name, string surname) { 
+            Name = name;
+            Surname = surname;
+        }
+        public string GetFullName() { 
+            return Surname + ", " + Name;
+        }
+    }
+}
