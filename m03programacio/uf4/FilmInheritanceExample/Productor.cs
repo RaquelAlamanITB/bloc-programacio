6cc119706cc14659cb21e13d65acf9409c12bc1d
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-31 23:03:34 +0100

    Update Productor.cs

--- m03programacio/uf4/FilmInheritanceExample/Productor.cs
+++ m03programacio/uf4/FilmInheritanceExample/Productor.cs
@@ -1,25 +1,15 @@
 ﻿using System;
-using System.Reflection.Metadata.Ecma335;
-using System.Xml.Linq;
-
 
 namespace oop
 {
     public class Productor : AbstractPerson, IPerson
     {
         //expression bodied property
-        public override string Name { get => name; set => name = value; }
+        public override string? Name { get; set; }
         //expression bodied property
-        public override string Surname { get => name; set => name = value; }
+        public override string? Surname { get ; set; }
 
-        public void DisplayName()
-        {
-            Console.WriteLine(Name);
-        }
-
-        public override string GetFullName()
-        {
-            return "Nom: " + Name;
-        }
+        public void DisplayName() => Console.WriteLine(Name);
+        public override string GetFullName() => "Nom: " + Name;
     }
 }
