714d0759aae3fe0337213328b81bceb579b2f27e
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-01-31 23:01:43 +0100

    Update AbstractPerson.cs

--- m03programacio/uf4/FilmInheritanceExample/AbstractPerson.cs
+++ m03programacio/uf4/FilmInheritanceExample/AbstractPerson.cs
@@ -4,9 +4,6 @@ namespace oop
 {
     public abstract class AbstractPerson
     {
-        protected string name;
-        protected string surname;
-
         public abstract string Name { get; set;  }
         public abstract string Surname { get; set; }
         public abstract string GetFullName();
