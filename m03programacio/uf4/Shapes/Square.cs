fb8e3db4688f5a75be4c9748aacf374865069643
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-02-20 19:41:52 +0100

    Update Square.cs

--- m03programacio/uf4/Shapes/Square.cs
+++ m03programacio/uf4/Shapes/Square.cs
@@ -4,7 +4,7 @@ namespace Shapes
 {
     public class Square : Shape2D
     {
-        public double Side;
+        public double Side { get; set; }
         public Square(double side, Colour foreColour, Colour backgroundColour) : base(foreColour, backgroundColour)
             => this.Side = side;
         public Square(Colour foreColour, Colour backgroundColour) : this(DefaultSize, foreColour, backgroundColour) { }
