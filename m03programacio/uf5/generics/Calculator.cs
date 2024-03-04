90511b40c84433f84227e41065a98bc6414fd6ad
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-03-04 12:52:27 +0100

    Create Calculator.cs

---
+++ m03programacio/uf5/generics/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace generics
+{
+    public class Calculator<T>
+    {
+        public static bool AreEqual(T value1, T value2)
+        {
+            return value1.Equals(value2);
+        }
+    }
+}
