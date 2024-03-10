bb5b078f21e358cb1fb999f99253af37ff3c15e7
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-03-10 23:46:14 +0100

    Add files via upload

---
+++ m03programacio/uf5/predicate/Program.cs
@@ -0,0 +1,30 @@
+﻿using collections;
+
+namespace MyNamespace
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var emails = new List<string>()
+           {
+                "admin@gmail.com",
+                "admin@gmail",
+                "2gmail.com",
+                "test@hotmail.com"
+            };
+
+            Predicate<string> predicate = new Predicate<string>(Helper.EmailIsValid);
+
+            var validEmails = emails.FindAll(predicate);
+
+            Console.WriteLine("Les adreces de correu vàlides són: ");
+
+            foreach (var email in validEmails)
+            {
+                Console.WriteLine(email);
+            }
+        }
+    }
+
+}
