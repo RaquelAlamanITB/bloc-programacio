1784022632314250918e784993f6afb602e4208e
RaquelAlamanITB <143938844+RaquelAlamanITB@users.noreply.github.com>
2024-03-12 23:28:26 +0100

    Add files via upload

---
+++ m03programacio/uf5/collections/Colleccio.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace collections
+{
+    public class MyClass 
+    {
+        public void MyMethod(int i)
+        {
+            Console.WriteLine("El valor passat com a paràmetre és: {0}", i);
+        }
+
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+}
